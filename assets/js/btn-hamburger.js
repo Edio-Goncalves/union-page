2f9aafe037bde6d0a4b03d19b5d0d82effed3ef7
Edio Goncalves <ediogoncalvesjr@hotmail.com>
2023-04-15 15:41:58 -0300

    Create the menu hamburger js

--- assets/js/btn-hamburger.js
+++ assets/js/btn-hamburger.js
@@ -1,7 +1,6 @@
-// const btnMobile = document.getElementById("btn-mobile");
-// function toggleMenu() {
-//   const nav = document.getElementById("nav");
-//   nav.classList.toggle("active");
-// }
+const btnMobile = document.querySelector("#btn-mobile");
 
-// btnMobile.addEventListener("click", toggleMenu);
+btnMobile.addEventListener("click", () => {
+  const nav = document.querySelector("#nav");
+  nav.classList.toggle("active");
+});
