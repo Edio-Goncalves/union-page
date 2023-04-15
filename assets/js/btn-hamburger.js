e102677362669968e06d3aa06c72dfcdf433668d
Edio Goncalves <ediogoncalvesjr@hotmail.com>
2023-04-15 15:12:50 -0300

    Create new menu

--- assets/js/btn-hamburger.js
+++ assets/js/btn-hamburger.js
@@ -1,7 +1,7 @@
-const btnMobile = document.getElementById("btn-mobile");
-function toggleMenu() {
-  const nav = document.getElementById("nav");
-  nav.classList.toggle("active");
-}
+// const btnMobile = document.getElementById("btn-mobile");
+// function toggleMenu() {
+//   const nav = document.getElementById("nav");
+//   nav.classList.toggle("active");
+// }
 
-btnMobile.addEventListener("click", toggleMenu);
+// btnMobile.addEventListener("click", toggleMenu);
